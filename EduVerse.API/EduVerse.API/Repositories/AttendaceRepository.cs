3ab6c7b46c7e75ba91b88d9d2cc2c8c8eef22bb5
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-04-12 13:45:31 +0300

    AttendanceEntity repository

---
+++ EduVerse.API/EduVerse.API/Repositories/AttendaceRepository.cs
@@ -0,0 +1,79 @@
+﻿namespace EduVerse.API.Repositories
+{
+    public class AttendaceRepository : IAttendanceRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendaceRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<AttendanceEntity?> GetAsync(int id)
+        {
+           return await _context.Attendances
+                .Where(a => a.Id == id)
+                .SingleOrDefaultAsync();
+        }
+
+        public async Task<IList<AttendanceEntity>> ListAsync()
+        {
+            return await _context.Attendances
+                .ToListAsync();
+        }
+
+        public async Task<int> AddAsync(AttendanceEntity attendance)
+        {
+            var transaction = await _context.Database.BeginTransactionAsync();
+
+            try
+            {
+                var result = await _context.Attendances.AddAsync(attendance);
+                await _context.SaveChangesAsync();
+                await transaction.CommitAsync();
+                return result.Entity.Id;
+            }
+            catch
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
+
+        public async Task<int> UpdateAsync(AttendanceEntity attendance)
+        {
+            var transaction = await _context.Database.BeginTransactionAsync();
+
+            try
+            {
+                _context.Entry(attendance).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                await transaction.CommitAsync();
+                return attendance.Id;
+            }
+            catch
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
+
+        public async Task<int> DeleteAsync(int id)
+        {
+            var transaction = await _context.Database.BeginTransactionAsync();
+
+            try
+            {
+                _context.Entry((await GetAsync(id)) !).State = EntityState.Deleted;
+                await _context.SaveChangesAsync();
+                await transaction.CommitAsync();
+                return id;
+            }
+            catch
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
+    }
+}
---
+++ EduVerse.API/EduVerse.API/Repositories/Interfaces/IAttendanceRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace EduVerse.API.Repositories.Interfaces
+{
+    public interface IAttendanceRepository
+    {
+        Task<AttendanceEntity?> GetAsync(int id);
+        Task<IList<AttendanceEntity>> ListAsync();
+        Task<int> AddAsync(AttendanceEntity attendance);
+        Task<int> UpdateAsync(AttendanceEntity attendance);
+        Task<int> DeleteAsync(int id);
+    }
+}
