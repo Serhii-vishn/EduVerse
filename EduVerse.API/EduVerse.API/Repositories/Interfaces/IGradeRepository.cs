81c5d7cb4b6cee04022d3148b0881095cf0bacbe
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-04-14 20:32:57 +0300

    grade repository

---
+++ EduVerse.API/EduVerse.API/Repositories/Interfaces/IGradeRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace EduVerse.API.Repositories.Interfaces
+{
+    public interface IGradeRepository
+    {
+        Task<GradeEntity?> GetAsync(int id);
+        Task<IList<GradeEntity>> ListAsync();
+        Task<int> AddAsync(GradeEntity grade);
+        Task<int> UpdateAsync(GradeEntity grade);
+        Task<int> DeleteAsync(int id);
+    }
+}
