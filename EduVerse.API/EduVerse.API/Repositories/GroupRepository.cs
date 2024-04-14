cddf6695ab2c22a3edfbfd5966f8c702fe9ede43
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-04-14 20:36:15 +0300

    group repositories

---
+++ EduVerse.API/EduVerse.API/Repositories/GroupRepository.cs
@@ -0,0 +1,78 @@
+﻿namespace EduVerse.API.Repositories
+{
+    public class GroupRepository : IGroupRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public GroupRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GroupEntity?> GetAsync(int id)
+        {
+            return await _context.Groups
+                 .Where(a => a.Id == id)
+                 .SingleOrDefaultAsync();
+        }
+
+        public async Task<IList<GroupEntity>> ListAsync()
+        {
+            return await _context.Groups
+                .ToListAsync();
+        }
+
+        public async Task<int> AddAsync(GroupEntity group)
+        {
+            var transaction = await _context.Database.BeginTransactionAsync();
+
+            try
+            {
+                var result = await _context.Groups.AddAsync(group);
+                await _context.SaveChangesAsync();
+                await transaction.CommitAsync();
+                return result.Entity.Id;
+            }
+            catch
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
+
+        public async Task<int> UpdateAsync(GroupEntity group)
+        {
+            var transaction = await _context.Database.BeginTransactionAsync();
+
+            try
+            {
+                _context.Entry(group).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                await transaction.CommitAsync();
+                return group.Id;
+            }
+            catch
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
+
+        public async Task<int> DeleteAsync(int id)
+        {
+            var transaction = await _context.Database.BeginTransactionAsync();
+
+            try
+            {
+                _context.Entry((await GetAsync(id)) !).State = EntityState.Deleted;
+                await _context.SaveChangesAsync();
+                await transaction.CommitAsync();
+                return id;
+            }
+            catch
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
+    }
+}
---
+++ EduVerse.API/EduVerse.API/Repositories/Interfaces/IGroupRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace EduVerse.API.Repositories.Interfaces
+{
+    public interface IGroupRepository
+    {
+        Task<GroupEntity?> GetAsync(int id);
+        Task<IList<GroupEntity>> ListAsync();
+        Task<int> AddAsync(GroupEntity group);
+        Task<int> UpdateAsync(GroupEntity group);
+        Task<int> DeleteAsync(int id);
+    }
+}
