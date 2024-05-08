9ac4616ece1514854b8f378961da49ee98043ec3
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-05-08 23:32:45 +0300

    Implement image service

---
+++ EduVerse.API/EduVerse.API/Services/Interfaces/IImageService.cs
@@ -0,0 +1,8 @@
+﻿namespace EduVerse.API.Services.Interfaces
+{
+    public interface IImageService
+    {
+        Task UploadPhotoAsync(IFormFile img, string fileFolder, string fileName);
+        Task DeletePhotoAsync(string fileFolder, string fileName);
+    }
+}
